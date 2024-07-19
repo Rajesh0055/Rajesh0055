d2c0ba4fc177a7f88b1ac7072090d2620f536e4c
Happy <omhshree@gmail.com>
2024-07-19 17:53:47 +0530

    fix: login page missing files

---
+++ src/app/login-page/login-page.component.ts
@@ -0,0 +1,14 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-login-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.scss'
+})
+export class LoginPageComponent  {
+
+  
+
+}
