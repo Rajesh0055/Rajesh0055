a5e76a6d665ff73c6998bfaf6cd8e8e9c7523d27
Happy <omhshree@gmail.com>
2024-07-21 18:55:39 +0530

    fix: userapi fix from error of nullinjctorerror

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,7 +6,7 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(),provideHttpClient()]
 };
 // export const appConfig: ApplicationConfig = {
 //   providers: [
