911a6f39878da86b0860abbd4350be2f2b18b85e
Happy <omhshree@gmail.com>
2024-07-30 11:52:31 +0530

    add on click pop up form

--- src/app/user-api.service.ts
+++ src/app/user-api.service.ts
@@ -13,4 +13,24 @@ export class UserApiService {
   getData(): Observable<any> {
     return this.http.get(this.apiUrl);
   }
+
+  postData(userName:any,firstName:any,lastName:any,emailId:any,phoneNo:any,address:any,createdOn:any,status:any): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+
+    var d={
+      "id": 0,
+      "userName": userName,
+      "firstName":firstName,
+      "lastName":lastName,
+      "emailId":emailId,
+      "phoneNo":phoneNo,
+      "address":address,
+      "createdOn":createdOn,
+      "status":status
+    }
+    return this.http.post<any>(`${this.apiUrl}/Create-User`, d, { headers });
+ 
+}
 }
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,25 +1,86 @@
 import { Component, OnInit } from '@angular/core';
 import { UserApiService } from '../user-api.service';
+import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,ReactiveFormsModule ],
   templateUrl: './users.component.html',
   styleUrl: './users.component.scss'
 })
 export class UsersComponent implements OnInit {
   data: any;
+  totalUsers:any;
+  dataForm: any;
   constructor(private apiService:UserApiService){}
+
+  openmodel(){
+    const modelDiv = document.getElementById('myModal');
+    if(modelDiv!= null){
+      modelDiv.style.display = 'block';
+    }
+  }
+  closemodel(){
+    const modelDiv = document.getElementById('myModal');
+    if(modelDiv!= null){
+      modelDiv.style.display = 'none';
+    }
+  }
+
+
 
   ngOnInit() {
     this.apiService.getData().subscribe(
       (response) => {
         this.data = response;
+        this.totalUsers=this.data.length;
         console.log(this.data);
       }
     );
+
+    this.dataForm = new FormGroup({
+      userName: new FormControl('', Validators.required),
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      emailId: new FormControl('', Validators.required),
+      phoneNo: new FormControl('', Validators.required),
+      address: new FormControl('', Validators.required),
+      createdOn: new FormControl('', Validators.required),
+      status: new FormControl('', Validators.required)
+    });
   
   }
 
-}
+  onSubmit() {
+
+    if (this.dataForm.valid) {
+      var id = this.dataForm.get("id").value;
+      var userName = this.dataForm.get("userName").value;
+      var firstName = this.dataForm.get("firstName").value;
+      var lastName = this.dataForm.get("lastName").value;
+      var emailId = this.dataForm.get("emailId").value;
+      var phoneNo = this.dataForm.get("phoneNo").value;
+      var address = this.dataForm.get("address").value;
+      var createdOn = this.dataForm.get("createdOn").value;
+      var status = this.dataForm.get("status").value;
+      
+      this.apiService.postData(userName,firstName,lastName,emailId,phoneNo,address,createdOn,status ).subscribe(
+        response => {
+          if(response.id>0)
+            {
+              alert("user created successfully");
+            }
+            else{
+              alert("Error");
+            }
+        }
+      );
+      }
+      
+  }
+  }
+
+
